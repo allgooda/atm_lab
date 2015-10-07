f79e60fcd42ad2a28292f1009df18e767776fdcc
Adam Allgood <allgoodadam@gmail.com>
2015-10-07 10:08:12 -0700

    atm lab additions

--- README.md
+++ README.md
@@ -1,3 +1,15 @@
 # ATM LAB
 
 In which we learn the steps to creating our own App.
+
+FULL APPLICATION STEPS
+
+1) Create a code rep
+2) Create First commit
+3) Back up on GH
+4) Make GH page presentable
+5) Start Dev server
+6) Connect Libraries 
+7) MODEL/Get model to work
+8)Identify and "cache" reference to the dom.
+9) Add event listener stubs.
--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,95 @@
 console.log("main js loaded.");
+
+var checking = 0;
+var savings = 0;
+
+
+
+var deposit = function(accountName, amountDeposited) {
+	if (accountName == "checking") {
+		checking = checking + amountDeposited;
+		return checking;
+	}
+	else {
+		savings = savings + amountDeposited;
+		return savings;
+	}
+};
+
+var withdraw = function(accountName, amountWithdrawn) {
+	if (accountName === 'checking') {
+		if (amountWithdrawn > checking) {
+			return checking;
+		}
+		else {
+			if (amountWithdrawn > savings) {
+				return alertUser(amountWithdrawn, savings)
+			}
+			savings = savings - amountWithdrawn;
+			return savings
+		}
+	}
+ }
+
+/*withdraw: which account, current balance, amount deposited
+-> new balance
+
+alertUser: message
+-> 'NSF'
+
+isEmpty: which account
+-> true or false*/
+
+//CACHE DOM REFERENCES
+var checkingBalanceEl = document.getElementById('checking-balance');
+var checkingInputEl = document.getElementById('checking-input');
+var checkingDespositButtonEl = document.getElementById('checking-deposit');
+
+//ADD EVENT LISTENERS
+
+
+
+var checkingDepositClick = function() {
+	console.log('clicked!')
+
+	// get amount from the dom
+	var amount = parseInt(checkingInputEl.value);
+	//deposit  amount to the checking balance
+	deposit("checking", amount);
+	//remove amount from the input
+	checkingInputEl.value = "";
+
+	//update the balance on the screen
+	render();
+}
+
+checkingDespositButtonEl.addEventListener('click', checkingDepositClick);
+
+
+var render = function() {
+	checkingInputEl.value = null;
+	checkingBalanceEl.innerHTML = "$" + checking;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
